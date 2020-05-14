fdc89123d8a417ed4b7294a1b6b11149b2075cdf
UntouchedOdin0 <UntouchedOdin0@users.noreply.github.com>
2020-05-14 22:34:03 +0100

    Update EnchantmentManager.java
    Little Changes

--- src/main/java/me/bristermitten/spigotexamples/customenchantment/EnchantmentManager.java
+++ src/main/java/me/bristermitten/spigotexamples/customenchantment/EnchantmentManager.java
@@ -3,6 +3,7 @@ package me.bristermitten.spigotexamples.customenchantment;
 import me.bristermitten.spigotexamples.reflect.MemberType;
 import me.bristermitten.spigotexamples.reflect.ReflectionCache;
 import me.bristermitten.spigotexamples.reflect.ReflectionDefinition;
+
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
@@ -20,105 +21,85 @@ import java.util.Set;
  * In charge of holding and managing the custom enchantments.
  * This should be an effective singleton (1 instance per Plugin).
  */
-public final class EnchantmentManager
-{
+public final class EnchantmentManager {
 
-    private static final ReflectionDefinition ACCEPTING_NEW_FIELD_DEF = new ReflectionDefinition(Enchantment.class, MemberType.FIELD, "acceptingNew");
-    private static final ReflectionDefinition BY_KEY_FIELD_DEF = new ReflectionDefinition(Enchantment.class, MemberType.FIELD, "byKey");
-    private static final ReflectionDefinition BY_NAME_FIELD_DEF = new ReflectionDefinition(Enchantment.class, MemberType.FIELD, "byName");
+	private static final ReflectionDefinition ACCEPTING_NEW_FIELD_DEF = new ReflectionDefinition(Enchantment.class, MemberType.FIELD, "acceptingNew");
+	private static final ReflectionDefinition BY_KEY_FIELD_DEF = new ReflectionDefinition(Enchantment.class, MemberType.FIELD, "byKey");
+	private static final ReflectionDefinition BY_NAME_FIELD_DEF = new ReflectionDefinition(Enchantment.class, MemberType.FIELD, "byName");
 
-    private final JavaPlugin plugin;
-    private final Set<Enchantment> customEnchantments;
+	private final JavaPlugin plugin;
+	private final Set<Enchantment> customEnchantments;
     private final BlastEnchantment blast;
+  
+	/**
+	 * Create a new EnchantmentManager.
+	 *
+	 * @param plugin the plugin managing the enchantments.
+	 */
+	public EnchantmentManager(JavaPlugin plugin) {
+		this.plugin = plugin;
+		this.customEnchantments = new HashSet<>();
 
-    /**
-     * Create a new EnchantmentManager.
-     *
-     * @param plugin the plugin managing the enchantments.
-     */
-    public EnchantmentManager(JavaPlugin plugin)
-    {
-        this.plugin = plugin;
-        this.customEnchantments = new HashSet<>();
+		this.blast = new Blast();
 
-        this.blast = new BlastEnchantment(plugin);
+		customEnchantments.add(blast);
+	}
 
-        customEnchantments.add(blast);
+	/**
+	 * Register all custom enchantments with Bukkit.
+	 * This uses reflection to hook into Bukkit's "acceptingNew" field to allow new Enchantments to be registered.
+	 * This method also registers any enchantments that are event handlers.
+	 * This method should only be called once per server startup.
+	 */
+	public void registerAllCustomEnchantments() {
+		try {
+			//Reflection to allow registering new enchants. It's locked by default.
+			Field acceptingField = (Field) ReflectionCache.get(ACCEPTING_NEW_FIELD_DEF);
+			acceptingField.set(null, true);
 
-    }
+			//Register all enchants here
+			for (Enchantment enchantment : customEnchantments) {
+				Enchantment.registerEnchantment(enchantment);
+				if (enchantment instanceof Listener) {
+					Bukkit.getPluginManager().registerEvents((Listener) enchantment, plugin);
+				}
+			}
+		} catch (Exception ex) {
+			plugin.getLogger().warning(("Could not register custom enchantments: " + ex));
+		} finally {
+			Enchantment.stopAcceptingRegistrations();
+		}
+	}
 
-    /**
-     * Register all custom enchantments with Bukkit.
-     * This uses reflection to hook into Bukkit's "acceptingNew" field to allow new Enchantments to be registered.
-     * This method also registers any enchantments that are event handlers.
-     * This method should only be called once per server startup.
-     */
-    public void registerAllCustomEnchantments()
-    {
-        try
-        {
-            //Reflection to allow registering new enchants. It's locked by default.
-            Field acceptingField = (Field) ReflectionCache.get(ACCEPTING_NEW_FIELD_DEF);
-            acceptingField.set(null, true);
+	/**
+	 * Reflectively unregister all custom enchantments from Bukkit, and their event handlers.
+	 */
+	public void unregisterAllCustomEnchantments() {
+		try {
+			Field byKeyField = (Field) ReflectionCache.get(BY_KEY_FIELD_DEF);
+			Field byNameField = (Field) ReflectionCache.get(BY_NAME_FIELD_DEF);
 
-            //Register all enchants here
-            for (Enchantment enchantment : customEnchantments)
-            {
-                Enchantment.registerEnchantment(enchantment);
-                if (enchantment instanceof Listener)
-                {
-                    Bukkit.getPluginManager().registerEvents((Listener) enchantment, plugin);
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            plugin.getSLF4JLogger().error("Could not register custom enchantments:", ex);
-        }
-        finally
-        {
-            Enchantment.stopAcceptingRegistrations();
-        }
-    }
+			@SuppressWarnings("unchecked") Map<NamespacedKey, Enchantment> byKey = (Map<NamespacedKey, Enchantment>) byKeyField.get(null);
+			@SuppressWarnings("unchecked") Map<String, Enchantment> byName = (Map<String, Enchantment>) byNameField.get(null);
 
-    /**
-     * Reflectively unregister all custom enchantments from Bukkit, and their event handlers.
-     */
-    public void unregisterAllCustomEnchantments()
-    {
-        try
-        {
-            Field byKeyField = (Field) ReflectionCache.get(BY_KEY_FIELD_DEF);
-            Field byNameField = (Field) ReflectionCache.get(BY_NAME_FIELD_DEF);
+			for (Enchantment enchantment : customEnchantments) {
+				//noinspection deprecation
+				byName.remove(enchantment.getName());
 
-            @SuppressWarnings("unchecked") Map<NamespacedKey, Enchantment> byKey = (Map<NamespacedKey, Enchantment>) byKeyField.get(null);
-            @SuppressWarnings("unchecked") Map<String, Enchantment> byName = (Map<String, Enchantment>) byNameField.get(null);
+				if (enchantment instanceof Listener) {
+					HandlerList.unregisterAll((Listener) enchantment);
+				}
+			}
+		} catch (Exception e) {
+			plugin.getLogger().warning("Could not unregister custom enchantments: " + e);
+		}
+	}
 
-            for (Enchantment enchantment : customEnchantments)
-            {
-                byKey.remove(enchantment.getKey());
-                //noinspection deprecation
-                byName.remove(enchantment.getName());
+	public BlastEnchantment getBlast() {
+		return blast;
+	}
 
-                if (enchantment instanceof Listener)
-                {
-                    HandlerList.unregisterAll((Listener) enchantment);
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            plugin.getSLF4JLogger().error("Could not unregister custom enchantments:", e);
-        }
-    }
-
-    public BlastEnchantment getBlast()
-    {
-        return blast;
-    }
-
-    public Set<Enchantment> getCustomEnchantments()
-    {
-        return Collections.unmodifiableSet(customEnchantments);
-    }
+	public Set<Enchantment> getCustomEnchantments() {
+		return Collections.unmodifiableSet(customEnchantments);
+	}
 }
